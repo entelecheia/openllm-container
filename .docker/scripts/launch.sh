5e22580742601b77b41aa4888986c88091f13ce3
Young Joon Lee <entelecheia@hotmail.com>
2023-10-02 16:02:08 +0900

    feat(docker): add conditional for app variant in launch.sh

--- .docker/scripts/launch.sh
+++ .docker/scripts/launch.sh
@@ -5,4 +5,16 @@ echo "Starting docker container..."
 # start ssh server
 sudo service ssh start
 # Clones the app repository from GitHub
+if [[ "${VARIANT}" == "app" ]]; then
+    if [ ! -d "${APP_INSTALL_ROOT}/${APP_CLONE_DIRNAME}" ]; then
+        # clone app repo
+        echo "Cloning app repo"
+        echo "from ${APP_SOURCE_REPO} to ${APP_INSTALL_ROOT}/${APP_CLONE_DIRNAME}"
+        git clone "https://github.com/${APP_SOURCE_REPO}.git" "$APP_INSTALL_ROOT/${APP_CLONE_DIRNAME}"
+    else
+        echo "App repo already cloned"
+    fi
+    # install app dependencies
+    # pip install -r $APP_INSTALL_ROOT/$APP_CLONE_DIRNAME/requirements.txt
+fi
 
